24694c4eb729aee8921eff834a0f2216fe801cb4
Dat Le <dat.le@gbst.com>
2014-11-13 17:01:48 +0700

    Hieu add 13/11/2014

--- src/Main.java
+++ src/Main.java
@@ -26,23 +26,91 @@ public class Main {
 		InputReader in = new InputReader(inputStream);
 		OutputWriter out = new OutputWriter(outputStream);
 		TaskTam solver = new TaskTam();
-		int testCount = Integer.parseInt(in.next());
-		for (int i = 1; i <= testCount; i++)
-			solver.solve(i, in, out);
+		solver.solve(1, in, out);
 		out.close();
 	}
 }
 
 class TaskTam {
+    public static int MAXN = 100001;
+
     public void solve(int testNumber, InputReader in, OutputWriter out) {
-        int count = in.readInt();
-        List<Long> divisors = IntegerUtils.getDivisors(count);
+        int[] divisorTable = IntegerUtils.generateDivisorTable(MAXN);
+        int[] primes = IntegerUtils.generatePrimes(MAXN);
+        int testCount = in.readInt();
+        for (int test = 0; test < testCount; test++) {
+            int count = in.readInt();
+            int mod = in.readInt();
+            long[] factor = new long[MAXN];
+            for (int i = count; i > (count + 1) / 2; i--) {
+                int cur = i;
+                int remain = count - i + 1;
+                int increase = cur;
+                int decrease = remain;
 
-        long answer = 0;
-        for (long x : divisors)
-            answer += x;
+                while (cur > 1) {
+                    factor[divisorTable[cur]] += increase;
+                    cur /= divisorTable[cur];
+                }
 
-        out.printLine(answer);
+                while (remain > 1) {
+                    factor[divisorTable[remain]] -= decrease;
+                    remain /= divisorTable[remain];
+                }
+            }
+
+            long[] answer = new long[count + 1];
+            Arrays.fill(answer, 1);
+
+            int mid = (count + 1) / 2;
+            for (int x : primes) {
+                answer[mid] *= IntegerUtils.power(x, factor[x], mod);
+                answer[mid] %= mod;
+                if (answer[mid] < 0)
+                    answer[mid] += mod;
+            }
+
+            answer[count - mid] = answer[mid];
+
+            for (int i = mid + 1; i <= count; i++) {
+                answer[i] = answer[i - 1];
+
+                List<Pair<Long, Integer>> curFact = factorize(count - i + 1);
+
+                for (Pair<Long, Integer> x : curFact) {
+                    answer[i] *= IntegerUtils.power(x.first, x.second, mod);
+                    answer[i] %= mod;
+                    if (answer[i] < 0)
+                        answer[i] += mod;
+                }
+
+                answer[count - i] = answer[i];
+            }
+
+            int queryCount = in.readInt();
+            for (int i = 0; i < queryCount; i++) {
+                int idx = in.readInt();
+                out.printLine(answer[idx]);
+            }
+        }
+    }
+
+    private List<Pair<Long, Integer>> factorize(long number) {
+        List<Pair<Long, Integer>> result = new ArrayList<Pair<Long, Integer>>();
+        int exponent = (int)number;
+        for (long i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                int power = 0;
+                do {
+                    power += exponent;
+                    number /= i;
+                } while (number % i == 0);
+                result.add(Pair.makePair(i, power));
+            }
+        }
+        if (number != 1)
+            result.add(Pair.makePair(number, exponent));
+        return result;
     }
 }
 
@@ -99,25 +167,10 @@ class InputReader {
         return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
     }
 
-    public String readString() {
-        int c = read();
-        while (isSpaceChar(c))
-            c = read();
-        StringBuffer res = new StringBuffer();
-        do {
-            res.appendCodePoint(c);
-            c = read();
-        } while (!isSpaceChar(c));
-        return res.toString();
-    }
-
     public interface SpaceCharFilter {
         public boolean isSpaceChar(int ch);
     }
 
-    public String next() {
-        return readString();
-    }
 }
 
 class OutputWriter {
@@ -152,41 +205,50 @@ class OutputWriter {
 
 class IntegerUtils {
 
-    public static List<Pair<Long, Integer>> factorize(long number) {
-        List<Pair<Long, Integer>> result = new ArrayList<Pair<Long, Integer>>();
-        for (long i = 2; i * i <= number; i++) {
-            if (number % i == 0) {
-                int power = 0;
-                do {
-                    power++;
-                    number /= i;
-                } while (number % i == 0);
-                result.add(Pair.makePair(i, power));
+    public static long power(long base, long exponent, long mod) {
+        if (exponent == 0)
+            return 1 % mod;
+        long result = power(base, exponent >> 1, mod);
+        result = result * result % mod;
+        if ((exponent & 1) != 0)
+            result = result * base % mod;
+        return result;
+    }
+
+    public static int[] generatePrimes(int upTo) {
+        boolean[] isPrime = generatePrimalityTable(upTo);
+        List<Integer> primes = new ArrayList<Integer>();
+        for (int i = 0; i < upTo; i++) {
+            if (isPrime[i])
+                primes.add(i);
+        }
+        return CollectionUtils.toArray(primes);
+    }
+
+    public static boolean[] generatePrimalityTable(int upTo) {
+        boolean[] isPrime = new boolean[upTo];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i < upTo; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < upTo; j += i)
+                    isPrime[j] = false;
             }
         }
-        if (number != 1)
-            result.add(Pair.makePair(number, 1));
-        return result;
+        return isPrime;
     }
 
-    public static List<Long> getDivisors(long number) {
-        List<Pair<Long, Integer>> primeDivisors = factorize(number);
-        return getDivisorsImpl(primeDivisors, 0, 1, new ArrayList<Long>());
-    }
-
-    private static List<Long> getDivisorsImpl(List<Pair<Long, Integer>> primeDivisors, int index, long current, List<Long> result)
-    {
-        if (index == primeDivisors.size()) {
-            result.add(current);
-            return result;
+    public static int[] generateDivisorTable(int upTo) {
+        int[] divisor = new int[upTo];
+        for (int i = 1; i < upTo; i++)
+            divisor[i] = i;
+        for (int i = 2; i * i < upTo; i++) {
+            if (divisor[i] == i) {
+                for (int j = i * i; j < upTo; j += i)
+                    divisor[j] = i;
+            }
         }
-        long p = primeDivisors.get(index).first;
-        int power = primeDivisors.get(index).second;
-        for (int i = 0; i <= power; i++) {
-            getDivisorsImpl(primeDivisors, index + 1, current, result);
-            current *= p;
-        }
-        return result;
+        return divisor;
     }
 
 }
@@ -231,5 +293,17 @@ class Pair<U, V> implements Comparable<Pair<U, V>> {
             return value;
         return ((Comparable<V>)second).compareTo(o.second);
     }
+}
+
+class CollectionUtils {
+
+    public static int[] toArray(Collection<Integer> collection) {
+        int[] array = new int[collection.size()];
+        int index = 0;
+        for (int element : collection)
+            array[index++] = element;
+        return array;
+    }
+
 }
 
