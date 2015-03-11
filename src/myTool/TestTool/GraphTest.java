d4139a41613f71c1614cf136b4422491c862b44d
trunghieu11 <vuondenthanhcong11@gmail.com>
2015-03-12 02:52:50 +0700

    New GraphTest

--- src/myTool/TestTool/GraphTest.java
+++ src/myTool/TestTool/GraphTest.java
@@ -1,8 +1,11 @@
 package myTool.TestTool;
 
 import myTool.StandardTool.MiscUtils;
+import myTool.StandardTool.Pair;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * Created by nthieu1 on 3/4/2015.
@@ -29,6 +32,32 @@ public class GraphTest {
             visited[curTo] = true;
         }
         MiscUtils.increaseByOne(from, to);
+        return new int[][]{from, to};
+    }
+
+    public static int[][] generateSimpleGraph(int vertexCount, int edgeCount) {
+        int[][] tree = generateTree(vertexCount);
+        Set<Pair<Integer, Integer>> graph = new HashSet<Pair<Integer, Integer>>();
+        for (int i = 0; i < vertexCount - 1; i++) {
+            graph.add(new Pair<Integer, Integer>(tree[0][i], tree[1][i]));
+        }
+        while (graph.size() < edgeCount) {
+            int from = random.nextInt(vertexCount) + 1;
+            int to = random.nextInt(vertexCount) + 1;
+            while (from == to || graph.contains(new Pair<Integer, Integer>(from, to)) || graph.contains(new Pair<Integer, Integer>(to, from))) {
+                from = random.nextInt(vertexCount) + 1;
+                to = random.nextInt(vertexCount) + 1;
+            }
+            graph.add(new Pair<Integer, Integer>(from, to));
+        }
+        int[] from = new int[edgeCount];
+        int[] to = new int[edgeCount];
+        int idx = 0;
+        for (Pair<Integer, Integer> x : graph) {
+            from[idx] = x.first;
+            to[idx] = x.second;
+            idx++;
+        }
         return new int[][]{from, to};
     }
 }
